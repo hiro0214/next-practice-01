2b1b590fae90030b264d957d20968b75138d5c09
hiro0214 <leonhurt2@gmail.com>
2022-02-09 17:30:27 +0900

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import Layout from '../components/Layout'
+
+export default function Home() {
+  return (
+    <Layout title='Home'>
+      <p className='texct-4xl'>Hello Nextjs</p>
+    </Layout>
+  )
+}
